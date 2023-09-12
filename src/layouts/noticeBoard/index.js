6f477ec046394c6abbb01c3115ef5c37eab68640
Himanshu9643 <himanshushokhanda123@gmail.com>
2023-09-12 12:07:39 +0530

    package

--- src/layouts/noticeBoard/index.js
+++ src/layouts/noticeBoard/index.js
@@ -1,9 +1,17 @@
 // @mui material components
-import { Box, Card, CardContent, CardMedia, IconButton, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  CardMedia,
+  IconButton,
+  Avatar,
+  Stack,
+  Typography,
+} from "@mui/material";
 import Grid from "@mui/material/Grid";
-import Icon from "@mui/material/Icon";
 import SoftBox from "components/SoftBox";
-import { Link } from "react-router-dom";
 import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 import DashboardNavbar from "examples/Navbars/DashboardNavbar";
 import Footer from "examples/Footer";
@@ -11,7 +19,12 @@ import { grey } from "@mui/material/colors";
 import AddIcon from "@mui/icons-material/Add";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
-import { Pagination } from 'swiper/modules';
+import { Pagination } from "swiper/modules";
+import { API_URL } from "config";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import SoftButton from "components/SoftButton";
+import { Link } from "react-router-dom";
 
 const CARD_PROPERTY = {
   borderRadius: 3,
@@ -19,6 +32,60 @@ const CARD_PROPERTY = {
 };
 
 function NoticeBoard() {
+  const [notice, setNotice] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/notices/list`);
+      if (response.data.length === 0) {
+        console.log("Please add notice");
+      } else {
+        setNotice(response.data);
+      }
+    } catch (error) {
+      console.log("There is some error in fetching data ", error);
+    }
+  };
+  console.log(notice, "notices");
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  function formatDate(dateString) {
+    const date = new Date(dateString);
+    const options = { weekday: "long", month: "long", day: "numeric" };
+    return date.toLocaleDateString("en-US", options);
+  }
+
+  function stringToColor(string) {
+    let hash = 0;
+    let i;
+
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    let color = "#";
+
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+    /* eslint-enable no-bitwise */
+
+    return color;
+  }
+
+  function stringAvatar(name) {
+    return {
+      sx: {
+        bgcolor: stringToColor(name),
+      },
+      children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+    };
+  }
+
   return (
     <DashboardLayout>
       <DashboardNavbar />
@@ -47,9 +114,12 @@ function NoticeBoard() {
                       Notice Board
                     </Typography>
                   </Box>
-                  <IconButton>
-                    <AddIcon />
-                  </IconButton>
+                  <Link to="/notice/add-notice" underline="none" color="primary">
+                    <SoftButton variant="gradient" color="dark">
+                      <AddIcon />
+                      &nbsp;add new notice
+                    </SoftButton>
+                  </Link>
                 </Box>
                 <Box sx={{ height: "1px", width: "100%", bgcolor: grey[100] }}></Box>
                 <CardContent sx={{ p: 3, mb: 0 }}>
@@ -65,107 +135,52 @@ function NoticeBoard() {
                         clickable: true,
                       }}
                       modules={[Pagination]}
-                      autoHeight={true}
+                      // autoHeight={true}
+                      style={{ height: "700px" }}
                       className="swiper-wrapper"
                     >
-                      <SwiperSlide>
-                        {/* Content for Slide 1 */}
-                        <CardMedia
-                          component="img"
-                          image="https://images.pexels.com/photos/904276/pexels-photo-904276.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                          sx={{
-                            borderRadius: 3,
-                            width: "100%",
-                            ml: 0,
-                            mt: 0,
-                            mb: 3,
-                            height: "400px",
-                          }}
-                        />
-                        <Typography variant="h5" sx={{ mb: 1, fontWeight: "bold" }}>
-                          News 1
-                        </Typography>
-                        <Typography variant="body1" color="text.secondary" sx={{ mb: 1 }}>
-                          14 May 2014
-                        </Typography>
-                        <Typography variant="body1" sx={{ mb: 3 }}>
-                          This impressive paella is a perfect party dish and a fun meal to cook
-                          together with your guests. Add 1 cup of froze. This impressive paella is a
-                          perfect party dish and a fun meal to cook together with your guests. Add 1
-                          cup of froze. This impressive paella is a perfect party dish and a fun
-                          meal to cook together with your guests. Add 1 cup of froze.
-                        </Typography>
-                      </SwiperSlide>
-                      <SwiperSlide>
-                        {/* Content for Slide 1 */}
-                        <CardMedia
-                          component="img"
-                          image="https://images.pexels.com/photos/904276/pexels-photo-904276.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                          sx={{
-                            borderRadius: 3,
-                            width: "100%",
-                            ml: 0,
-                            mt: 0,
-                            mb: 3,
-                            height: "400px",
-                          }}
-                        />
-                        <Typography variant="h5" sx={{ mb: 1, fontWeight: "bold" }}>
-                          News 1
-                        </Typography>
-                        <Typography variant="body1" color="text.secondary" sx={{ mb: 1 }}>
-                          14 May 2014
-                        </Typography>
-                        <Typography variant="body1" sx={{ mb: 3 }}>
-                          This impressive paella is a perfect party dish and a fun meal to cook
-                          together with your guests. Add 1 cup of froze.
-                        </Typography>
-                      </SwiperSlide>
-                      <SwiperSlide>
-                        {/* Content for Slide 1 */}
-                        <CardMedia
-                          component="img"
-                          image="https://images.pexels.com/photos/904276/pexels-photo-904276.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                          sx={{
-                            borderRadius: 3,
-                            width: "100%",
-                            ml: 0,
-                            mt: 0,
-                            mb: 3,
-                            height: "400px",  
-                          }}
-                        />
-                        <Typography variant="h5" sx={{ mb: 1, fontWeight: "bold" }}>
-                          News 1
-                        </Typography>
-                        <Typography variant="body1" color="text.secondary" sx={{ mb: 1 }}>
-                          14 May 2014
-                        </Typography>
-                        <Typography variant="body1" sx={{ mb: 3 }}>
-                          This impressive paella is a perfect party dish and a fun meal to cook
-                          together with your guests. Add 1 cup of froze.
-                        </Typography>
-                      </SwiperSlide>
+                      {notice.map((item) => {
+                        return (
+                          <SwiperSlide key={item._id}>
+                            <CardMedia
+                              component="img"
+                              image={item.imgPath}
+                              sx={{
+                                borderRadius: 3,
+                                width: "100%",
+                                ml: 0,
+                                mt: 0,
+                                mb: 3,
+                                height: "350px",
+                              }}
+                            />
+                            <Stack
+                              direction="row"
+                              justifyContent="start"
+                              alignItems="center"
+                              spacing={2}
+                            >
+                              <Avatar {...stringAvatar(item.heading)} />
+                              <Box>
+                                <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+                                  {item.heading}
+                                </Typography>
+                                <Typography variant="body1" color="text.secondary" sx={{ mb: 1 }}>
+                                  {item.noticeDate ? formatDate(item.noticeDate) : "Date not found"}
+                                </Typography>
+                              </Box>
+                            </Stack>
+                            <Typography
+                              variant="body1"
+                              sx={{ my: 2, overflowY: "auto", height: "250px" }}
+                            >
+                              {item.description}
+                            </Typography>
+                          </SwiperSlide>
+                        );
+                      })}
                     </Swiper>
                   </Box>
-                  {/* <CardMedia
-                    component="img"
-                    image="https://images.pexels.com/photos/904276/pexels-photo-904276.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-                    sx={{ borderRadius: 3, width: "100%", ml: 0, mt: 0, mb: 3 }}
-                  />
-                  <Typography variant="h5" sx={{ mb: 1, fontWeight: "bold" }}>
-                    News
-                  </Typography>
-                  <Typography variant="body1" color="text.secondary" sx={{ mb: 1 }}>
-                    14 May 2014
-                  </Typography>
-                  <Typography variant="body1" sx={{ mb: 3 }}>
-                    This impressive paella is a perfect party dish and a fun meal to cook together
-                    with your guests. Add 1 cup of froze . This impressive paella is a perfect party
-                    dish and a fun meal to cook together with your guests. Add 1 cup of froze This
-                    impressive paella is a perfect party dish and a fun meal to cook together with
-                    your guests. Add 1 cup of froze
-                  </Typography> */}
                 </CardContent>
               </Card>
             </Grid>
