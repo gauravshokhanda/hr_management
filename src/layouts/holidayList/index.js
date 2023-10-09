5bcf7ce5f879bdb9e5c9a246a050089861a1c467
Himanshu9643 <himanshushokhanda123@gmail.com>
2023-10-09 17:15:04 +0530

    Holiday

--- src/layouts/holidayList/index.js
+++ src/layouts/holidayList/index.js
@@ -16,7 +16,11 @@ import SoftInput from "components/SoftInput";
 import SoftTypography from "components/SoftTypography";
 import axios from "axios";
 import { API_URL } from "config";
-import { Switch } from "@mui/material";
+import { Switch, Typography } from "@mui/material";
+import moment from "moment";
+import HolidayTable from "./holidayTable";
+import "./cell.css";
+import SoftButton from "components/SoftButton";
 
 function HolidayList() {
   const [weekendsVisible, setWeekendsVisible] = useState(true);
@@ -27,9 +31,10 @@ function HolidayList() {
   const [eventTitle, setEventTitle] = useState("");
   const [clickedEventInfo, setClickedEventInfo] = useState(null);
   const [holiday, setIsHoliday] = useState(false);
+  const [showTable, setShowTable] = useState(true);
+  const [showCalendar, setShowCalendar] = useState(false);
 
   const calendarRef = useRef(null);
-
 
   const handleEventClick = (clickInfo) => {
     const event = clickInfo.event;
@@ -42,10 +47,18 @@ function HolidayList() {
     setCreateEventModalOpen(true);
   };
 
+  const changeTable = () => {
+    setShowTable(!showTable);
+    setShowCalendar(false);
+  };
+  const changeCalendar = () => {
+    setShowCalendar(!showCalendar);
+    setShowTable(false);
+  };
 
   const handleCreateEvent = async () => {
     const calendarApi = calendarRef.current.getApi();
-  
+
     if (selectedDate && eventTitle) {
       const newEvent = {
         title: eventTitle,
@@ -54,14 +67,14 @@ function HolidayList() {
         allDay: selectedDate.allDay,
         holiday,
       };
-  
+
       try {
         // Make a POST request to save the event
         const response = await axios.post(`${API_URL}/holiday/holiday-list`, newEvent);
-  
+
         if (response.status === 200) {
           const data = response.data;
-  
+
           // Update the currentEvents state with the new event using the callback form
           setCurrentEvents((prevEvents) => [
             ...prevEvents,
@@ -73,7 +86,7 @@ function HolidayList() {
               allDay: data.allDay,
             },
           ]);
-  
+
           setCreateEventModalOpen(false);
           setSelectedDate(null);
           setEventTitle("");
@@ -87,11 +100,10 @@ function HolidayList() {
     }
   };
 
-  
   const fetchEvents = async () => {
     try {
       const response = await axios.get(`${API_URL}/holiday/holiday-list`);
-  
+
       if (response.status === 200) {
         const eventData = response.data;
 
@@ -101,27 +113,46 @@ function HolidayList() {
       console.error("There is some error", error);
     }
   };
-  
+
   useEffect(() => {
     fetchEvents();
   }, []);
-  
-  
+
+  const handleEventDrop = async (dropInfo) => {
+    const event = dropInfo.event;
+    const updatedEvent = {
+      start: event.start.toISOString(),
+      end: event.end.toISOString(),
+      allDay: event.allDay,
+      holiday: event.holiday,
+    };
+
+    try {
+      // Make a PUT request to update the event on the server
+      const response = await axios.put(`${API_URL}/holiday/holiday-list/${event.id}`, updatedEvent);
+
+      if (response.status === 200) {
+        console.log("Event updated successfully");
+      } else {
+        console.error("Failed to update event:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error updating event:", error);
+    }
+  };
 
   const handleDeleteEvent = async () => {
     const event = clickedEventInfo.event;
 
-    const id = event._def.publicId
+    const id = event._def.publicId;
 
-    try{
-      const response = await axios.delete(`${API_URL}/holiday/holiday-list/${id}`,)
+    try {
+      const response = await axios.delete(`${API_URL}/holiday/holiday-list/${id}`);
 
-      if(response.status === 200){
+      if (response.status === 200) {
         console.log("Deleted successfully");
       }
-
-    }
-    catch(error){
+    } catch (error) {
       console.error("There is someee  error", error);
     }
     setDeleteEventModalOpen(false);
@@ -134,103 +165,157 @@ function HolidayList() {
   const closeDeleteEventModal = () => {
     setDeleteEventModalOpen(false);
   };
-  
-  const transformedEvents =   currentEvents?.map((event) => ({
+
+  const checkIfHoliday = (date, transformedEvents) => {
+    for (const event of transformedEvents) {
+      if (moment(date).format("LL") === moment(event.start).format("LL")) {
+        if (event.extendedProps.holiday) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  };
+
+  const transformedEvents = currentEvents?.map((event) => ({
     title: event.title,
     start: event.start,
     end: event.end,
-    backgroundColor: event.holiday ? 'green' : 'red',
-    id:event._id
+    extendedProps: {
+      holiday: event.holiday ? true : false,
+    },
+    color: event.holiday ? "green" : "",
+    textColor: event.holiday ? "#000" : "#000",
+    id: event._id,
   }));
+
+  const customDayCellClassNames = (arg) => {
+    const cellDate = arg.date;
+    const isHoliday = checkIfHoliday(cellDate, transformedEvents);
+
+    if (isHoliday) {
+      return "custom-holiday-cell";
+    }
+
+    return "";
+  };
 
   return (
     <DashboardLayout>
       <DashboardNavbar />
-      <SoftBox mt={5} mb={3}>
-        <FullCalendar
-          ref={calendarRef}
-          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-          headerToolbar={{
-            left: "prev,next today",
-            center: "title",
-            right: "dayGridMonth,timeGridWeek,timeGridDay",
-          }}
-          initialView="dayGridMonth"
-          editable={true}
-          selectable={true}
-          selectMirror={true}
-          dayMaxEvents={true}
-          weekends={weekendsVisible}
-          events={transformedEvents}
-          select={handleDateSelect}
-          eventClick={handleEventClick}
-        />
+      <SoftBox mt={5} display="flex" alignItems="center" justifyContent="space-between">
+        <Typography variant="h3" fontWeight={700}>
+          Holiday {showTable ? "Table" : "Calendar"}
+        </Typography>
+        <SoftBox display="flex" gap="20px">
+          <SoftButton color={showTable ? "secondary" : null} onClick={changeTable}>
+            Table
+          </SoftButton>
+          <SoftButton color={showCalendar ? "secondary" : null} onClick={changeCalendar}>
+            Calendar
+          </SoftButton>
+        </SoftBox>
       </SoftBox>
-      <Footer />
-      <Modal open={deleteEventModalOpen} onClose={closeDeleteEventModal}>
-        <Box
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            width: 400,
-            bgcolor: "background.paper",
-            boxShadow: 24,
-            p: 4,
-          }}
-        >
-          <SoftTypography sx={{ mb: 2 }} variant="h4">
-            Delete Event
-          </SoftTypography>
-          <p>Are you sure you want to delete the event?</p>
-          <Button sx={{ mt: 2, mr: 2 }} variant="contained" onClick={closeDeleteEventModal}>
-            Cancel
-          </Button>
-          <Button sx={{ mt: 2 }} variant="contained" color="secondary" onClick={handleDeleteEvent}>
-            Delete
-          </Button>
-        </Box>
-      </Modal>
-      <Modal open={createEventModalOpen} onClose={closeCreateEventModal}>
-        <Box
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            width: 400,
-            bgcolor: "background.paper",
-            boxShadow: 24,
-            p: 4,
-          }}
-        >
-          <SoftTypography sx={{ mb: 2 }} variant="h4">
-            Create Event
-          </SoftTypography>
-          <SoftInput
-            placeholder="Event Title"
-            fullWidth
-            value={eventTitle}
-            onChange={(e) => setEventTitle(e.target.value)}
-          />
-          <div>
-            <label>Is Holiday:</label>
-            <Switch checked={holiday} onChange={() => setIsHoliday(!holiday)} />
-          </div>
-          <Button
-            sx={{ mt: 2 }}
-            variant="contained"
-            color="secondary"
-            onClick={closeCreateEventModal}
-          >
-            Cancel
-          </Button>
-          <Button sx={{ mt: 2, ml: 2 }} variant="contained" onClick={handleCreateEvent}>
-            Save
-          </Button>
-        </Box>
-      </Modal>
+      {showTable ? (
+        <>
+          <SoftBox mt={5} mb={3}>
+            <FullCalendar
+              ref={calendarRef}
+              plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+              headerToolbar={{
+                left: "prev,next today",
+                center: "title",
+                right: "dayGridMonth,timeGridWeek,timeGridDay",
+              }}
+              initialView="dayGridMonth"
+              editable={true}
+              selectable={true}
+              selectMirror={true}
+              dayMaxEvents={true}
+              eventDisplay="box"
+              weekends={weekendsVisible}
+              events={transformedEvents}
+              select={handleDateSelect}
+              eventClick={handleEventClick}
+              dayCellClassNames={customDayCellClassNames}
+              eventDrop={handleEventDrop}
+            />
+          </SoftBox>
+          <Footer />
+          <Modal open={deleteEventModalOpen} onClose={closeDeleteEventModal}>
+            <Box
+              sx={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: 400,
+                bgcolor: "background.paper",
+                boxShadow: 24,
+                p: 4,
+              }}
+            >
+              <SoftTypography sx={{ mb: 2 }} variant="h4">
+                Delete Event
+              </SoftTypography>
+              <p>Are you sure you want to delete the event?</p>
+              <Button sx={{ mt: 2, mr: 2 }} variant="contained" onClick={closeDeleteEventModal}>
+                Cancel
+              </Button>
+              <Button
+                sx={{ mt: 2 }}
+                variant="contained"
+                color="secondary"
+                onClick={handleDeleteEvent}
+              >
+                Delete
+              </Button>
+            </Box>
+          </Modal>
+          <Modal open={createEventModalOpen} onClose={closeCreateEventModal}>
+            <Box
+              sx={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: 400,
+                bgcolor: "background.paper",
+                boxShadow: 24,
+                p: 4,
+              }}
+            >
+              <SoftTypography sx={{ mb: 2 }} variant="h4">
+                Create Event
+              </SoftTypography>
+              <SoftInput
+                placeholder="Event Title"
+                fullWidth
+                value={eventTitle}
+                onChange={(e) => setEventTitle(e.target.value)}
+              />
+              <div>
+                <label>Is Holiday:</label>
+                <Switch checked={holiday} onChange={() => setIsHoliday(!holiday)} />
+              </div>
+              <Button
+                sx={{ mt: 2 }}
+                variant="contained"
+                color="secondary"
+                onClick={closeCreateEventModal}
+              >
+                Cancel
+              </Button>
+              <Button sx={{ mt: 2, ml: 2 }} variant="contained" onClick={handleCreateEvent}>
+                Save
+              </Button>
+            </Box>
+          </Modal>
+        </>
+      ) : (
+        <HolidayTable handleDeleteEvent />
+      )}
     </DashboardLayout>
   );
 }
